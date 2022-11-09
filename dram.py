7bd2741b03a793d4c07cbc2e3464f88bd1a8fef1
zhang677 <ZhangghLCN@163.com>
2022-11-09 13:52:27 +0000

    Fix code

--- dram.py
+++ dram.py
@@ -3,45 +3,6 @@ from scipy.io import mmwrite,mmread
 from scipy.sparse import coo_array
 from scipy import sparse
 
-def exchange(row,col):
-  return (col,row)
-
-def get_csr(file_path):
-  coo = mmread(file_path)
-  return coo.tocsr()
-
-def extract(csr):
-  (row, col) = csr.shape
-  avg_degree = int(csr.nnz / col + 1)
-  return (row, col, avg_degree, round((col/(row+col)),4))
-
-def generate(seed, row, col, avg_degree):
-  (row,col) = exchange(row, col)
-  np.random.seed(seed)
-  base = np.ones(row) * avg_degree
-  tile = np.random.randint(-avg_degree, avg_degree, row) 
-  for i in range(row):
-    base[i] = base[i] + tile[i]
-  v_list = []
-  e_list = []
-  nnz = 0
-  for i in range(row):
-    cur_row = i
-    non_zero = np.random.randint(0, col, int(base[i]))
-    for r in range(int(base[i])):
-      nnz = nnz + 1
-      v_list.append(cur_row)
-      e_list.append(non_zero[r])
-  data = np.ones(nnz)
-  avg_degree = int(nnz / row + 1)
-  sparsecsr = coo_array((data, (v_list, e_list)), shape=(row, col)).tocsr().transpose()
-  return (sparsecsr, avg_degree)
-
-def generate_csr(seed, row, col, avg_degree, name):
-  (csr,deg) = generate(seed, row, col, avg_degree)
-  mmwrite(name, csr)
-  print((row,col,avg_degree,deg))
-
 # "G": Gustavson, "O": Outerloop, "I": Innerloop
 # C(i,j) = A(i,k) * B(k,j)
 def linear_cache_model(dram_max,dram_min,Q,q):
@@ -125,15 +86,13 @@ def get_dram_estimate(coo, cache):
   dram = {"G":[G["max"][0],linear_cache_model(G["max"][1],G["min"][1],G["min"][1],G["q"]),linear_cache_model(G["max"][2],G["min"][2],G["min"][2],G["q"])]}
   dram["O"] = [O["max"][0],linear_cache_model(O["max"][1],O["min"][1],O["min"][1],O["q"]),linear_cache_model(O["max"][2],O["min"][2],O["min"][2],O["q"])]
   dram["I"] = [linear_cache_model(I["max"][0],I["min"][0],I["min"][0],I["q"]),linear_cache_model(I["max"][1],I["min"][1],I["min"][1],I["q"]),I["max"][2]]
+  return dram
 
 
 if __name__ == "__main__":
-  seed = 42
   root = '/home/nfs_data/zhanggh/HyperGsys/HyperGsys/data/mtx_data/KDD/'
-  #name = 'DAWN-unique-hyperedges.mtx'
-  #print(extract(get_csr(root+name)))
-  name = 'test1.mtx'
-  row = 2559
-  col = 141087
-  avg_degree = 4
-  generate_csr(seed, row, col, avg_degree, root+name)
+  name = 'DAWN-unique-hyperedges.mtx'
+  file_path = root + name
+  coo = mmread(file_path)
+  dram = get_dram_estimate(coo, coo.nnz / 2)
+  print(dram)
